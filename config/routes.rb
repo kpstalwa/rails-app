1fd10f0ec2e036d3f43e8760b530c67247d43d82
Karam Talwandi <k.talwandi@hotmail.com>
2017-12-22 18:24:20 -0500

    Implement sign-up for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/help'=> 'static_pages#help'
   get 'static_pages/about'=> 'static_pages#about'
   get 'static_pages/contact'=> 'static_pages#contact'
-  resource :users
+  resources :users
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
